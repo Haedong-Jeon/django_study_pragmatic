96f2885c1e2ae93249ab169dfb8097ad1cdb2185
Haedong-Jeon <haedongjeon@icloud.com>
2021-02-14 17:20:29 +0900

    버그 픽스

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from .forms import Profile, ProfileCreationForm
 from .decorators import profile_ownership_required
@@ -8,15 +9,18 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class =ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
         temp_profile = form.save(commit=False)
         temp_profile.user = self.request.user
         temp_profile.save()
-
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk':self.object.user.pk})
+
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -26,3 +30,6 @@ class ProfileUpdateView(UpdateView):
     form_class =ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk':self.object.user.pk})

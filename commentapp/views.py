5b4022fde2b470d3bbf46e7e45c92c608fe11ba1
Haedong-Jeon <haedongjeon@icloud.com>
2021-02-15 19:35:56 +0900

    댓글 완성

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 
 from .models import Comment
 from .forms import CommentCreationForm
+from .decorators import comment_ownership_required
 from articleapp.models import Article
 
 class CommentCreateView(CreateView):
@@ -13,7 +15,7 @@ class CommentCreateView(CreateView):
 
     def form_valid(self, form):
         temp_comment = form.save(commit=False)
-        temp_comment.article = Article.objects.get(pk=self.requset.POST['article_pk'])
+        temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])
         temp_comment.writer = self.request.user
         temp_comment.save()
         
@@ -21,4 +23,17 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
-        return super().get_success_url()
+ 
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
+
+
+
+

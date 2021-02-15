6b82ea70da714a1944a8397c24d1ad2279152960
Haedong-Jeon <haedongjeon@icloud.com>
2021-02-15 12:21:38 +0900

    게시글 생성, 삭제, 수정

---
+++ articleapp/forms.py
@@ -0,0 +1,7 @@
+from .models import Article
+from django.forms import ModelForm
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content']

f36144604591a62b4c6e311ed761c5d386b81c44
Haedong-Jeon <haedongjeon@icloud.com>
2021-02-15 14:03:11 +0900

    *bug fix
    - 오타

--- articleapp/views.py
+++ articleapp/views.py
@@ -51,4 +51,4 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginated_by = 5
+    paginate_by = 5
